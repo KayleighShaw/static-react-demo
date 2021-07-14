4ac0e5d91bc1bf43f30ef2d154d7f690ed9f844e
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-07-14 14:43:46 +0100

    Create cloudfront

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -23,6 +25,10 @@ export class CdkStack extends cdk.Stack {
       sources: [s3Deployment.Source.asset('../build')],
     });
 
+    const distribution = new cloudfront.Distribution(this, 'ks-static-react-app-distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(bucket)}
+    })
+
     //Add permission boundary (prevents us doing insecure things)
     const boundary = iam.ManagedPolicy.fromManagedPolicyArn(this, 'Boundary', `arn:aws:iam::${process.env.AWS_ACCOUNT}:policy/ScopePermissions`);
 
@@ -32,6 +38,10 @@ export class CdkStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'Bucket URL', {
       value: bucket.bucketDomainName,
+    })
+
+    new cdk.CfnOutput(this, 'CloudFront URL', {
+      value: distribution.distributionDomainName,
     })
   }
 }
